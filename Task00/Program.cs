f6e0a913583a1e70a1b8a14e64b9282bf45610d0
BatmanovSergey <xoolly@mail.ru>
2022-10-14 19:39:11 +0300

    Дополнения

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -11,6 +11,10 @@
 Console.Write("Введите целое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
+// int num1 = Int.TryParse - проверка
+
+// int num1 = Int.Parse(Console.ReadLine()); - анаог Convert.ToInt32 но более простой
+
 // string str = Console.ReadLine();
 // int n = Convert.ToInt32(str)
 
