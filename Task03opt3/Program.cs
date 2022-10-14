251f771722fd9c44dd27ea458f3eca9fc28968df
BatmanovSergey <xoolly@mail.ru>
2022-10-14 23:49:54 +0300

    add task03opt3 корректировка

--- Task03opt3/Program.cs
+++ Task03opt3/Program.cs
@@ -11,30 +11,30 @@ int num = Convert.ToInt32(Console.ReadLine());
 
 switch (num)
 {
-    case (1):
+    case 1:
      Console.WriteLine("Понедельник");
         break;
 
-    case (2):
+    case 2:
      Console.WriteLine("Вторник");
         break;
 
-    case (3):
+    case 3:
      Console.WriteLine("Среда");
         break;
-    case (4):
+    case 4:
         Console.WriteLine("Четверг");
         break;
 
-    case (5):
+    case 5:
      Console.WriteLine("Пятница");
         break;
 
-    case (6):
+    case 6:
        Console.WriteLine("Суббота");
         break;
 
-    case (7):
+    case 7:
     Console.WriteLine("Воскресенье");
         break;
 
