593e211ed75ea50183abc3edceac56bfae8fed53
BatmanovSergeyg <xoolly@mail.ru>
2022-10-15 02:11:33 +0300

    add task02

---
+++ Task02/Program.cs
@@ -0,0 +1,15 @@
+﻿// задача 02
+// Напишите программу, которая на вход принимает два числа и выдаёт
+// какое число большое и какое меньшее
+
+
+Console.Write("Введите целое число1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите целое число2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+    Console.Write($"MAX = {num1} и MIN = {num2}");
+else
+    Console.Write($"MAX = {num2} и MIN = {num}");
