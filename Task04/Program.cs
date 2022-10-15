3ba7e7c00440b8e0b3d7a329c2308e7fb4a9cc92
BatmanovSergeyg <xoolly@mail.ru>
2022-10-15 03:02:29 +0300

    add task04

---
+++ Task04/Program.cs
@@ -0,0 +1,12 @@
+﻿// задача 04
+// Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное их этих чисел
+
+Console.Write("Введите целое число1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите целое число2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите целое число3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
