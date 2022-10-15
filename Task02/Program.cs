a98c82874a1188d13a79c2c4afa6b8a8781f2ba5
BatmanovSergeyg <xoolly@mail.ru>
2022-10-15 12:44:23 +0300

    корретировка Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -9,6 +9,7 @@ Console.Write("Введите целое число2: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 if (num1 > num2)
+
     Console.Write($"MAX = {num1} и MIN = {num2}");
 else
     Console.Write($"MAX = {num2} и MIN = {num1}");
