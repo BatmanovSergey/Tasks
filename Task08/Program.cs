76fc79a09cbdf951e14f1f8e5fb0761aaaee8aee
BatmanovSergeyg <xoolly@mail.ru>
2022-10-16 22:44:19 +0300

    Добавили Task 08

---
+++ Task08/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 08
+// Напишите программу, которая на вход принимает число N,
+// а на выходе показывает всё чётные числа от 1 до N.
+
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 1)
+    Console.WriteLine("Недопустимое число");
+else
+{
+    int count = 2;
+    while (count <= num)
+    {
+        Console.Write($"{count} ");
+        count = count + 2;
+    }
+}
