aae2d6e8ab5d4ed4801cd8127526f61cc06095db
BatmanovSergeyg <xoolly@mail.ru>
2022-10-18 22:34:16 +0300

    корректирвока Task11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -5,24 +5,25 @@
 // 782 -> 72
 // 918 -> 98
 
-// int num = new Random().Next(100, 1000);
-// Console.WriteLine($"Случайное число из диапазаона 100-1000 {num}");
+int num = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из диапазаона 100-1000 {num}");
 
-// int first = num / 100; 
-// int second = num % 10;
-// int number = first*10+second;
+//Решение простое
 
-// Console.WriteLine(number);
+int first = num / 100;
+int second = num % 10;
+int answer = first * 10 + second;
 
-int FirstLastNumber(int number)
+Console.WriteLine(answer);
+
+// Решение через создание функции (метода)
+
+int FirstLastNumber(int number) // создаем метод
 {
     int firstNumber = number / 100;
     int lastNumber = number % 10;
     return firstNumber*10 + lastNumber;
  }
-
-int num = new Random().Next(100, 1000);
-
-Console.WriteLine(num);
-int final = FirstLastNumber(num); //я дописывал
+ 
+int final = FirstLastNumber(num); //вызываем метод и передадаём в него нащу рандомную цифру
 Console.WriteLine(final);
