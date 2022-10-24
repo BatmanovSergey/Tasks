c522efc1e70ceb8df4f7f7835fa7e6ce6bb7662f
BatmanovSergeydotnet <xoolly@mail.ru>
2022-10-25 00:22:09 +0300

    add Task19

--- task19/Program.cs
+++ task19/Program.cs
@@ -6,3 +6,29 @@
 // 23432 -> да
 // 12821 -> да
 
+bool FiveDigit(int number) // метод 1
+{
+    return (number > 9999 || number < -9999);
+}
+
+int Palindrom(int number) // метод 2
+{
+    int first = number / 10000;
+    int fifth = number % 10;
+    int fourth = number % 10 % 10;
+    int second = number % 10 % 10 % 10 % 10;
+    if (first == fifth && second == fourth) return number;
+    else return -1;
+}
+
+Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool fiveDigit = FiveDigit(num);
+if (fiveDigit)
+{
+    int palindrom = Palindrom(num);
+    Console.WriteLine(palindrom == num ? $"Да, число {num} является палиндромом"
+                                       : $"нет, число {num} не является палиндромом");
+}
+else Console.WriteLine($"Нет, число {num} не является пятизначным");
