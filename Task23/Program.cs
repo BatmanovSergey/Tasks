900bd80629138afec6f443c9ef1f54dc6b0f7f24
BatmanovSergeydotnet <xoolly@mail.ru>
2022-10-25 01:57:40 +0300

    add Task23opt

---
+++ Task23/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 23. Домашняя работа
+// Напишите программу, которая
+// принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+
+// Простое решение
+
+Console.Write("Введите натуральное число1: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+if (num <= 0) Console.WriteLine($"Некорректное число");
+else Table(num);
+
+void Table(int number)
+{
+    int count = 1;
+    while (count <= number)
+    {
+        Console.WriteLine($"{count,4} || {count * count * count,4}");
+        count++;
+    }
+}
---
+++ Task23opt1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 23. Домашняя работа
+// Напишите программу, которая
+// принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+
+// Решение через переменную double  с использованием встроенного метода Math.Pow
+
+Console.Write("Введите натуральное число: ");
+double num = Convert.ToInt32(Console.ReadLine());
+
+if (num <= 0) Console.WriteLine($"Некорректное число");
+else Table1(num);
+
+void Table1(double number) 
+{ 
+        double count = 1;
+        while (count <= number)
+        {           
+           Console.WriteLine($"{count, 4} | {Math.Pow(count, 3), 4}");
+           count++;
+        }
+}
