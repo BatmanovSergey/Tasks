d61517c3cdde1bc86ce880cbd9fe513d9ec60bab
BatmanovSergeydotnet <xoolly@mail.ru>
2022-10-25 15:55:04 +0300

    Корректировка Task16

--- task16/Program.cs
+++ task16/Program.cs
@@ -12,7 +12,7 @@ bool Square(int number1, int number2) // Наш метод
     return number1 * number1 == number2 || number2 * number2 == number1;
 }
 
-Console.Write("Введите первое число: ")
+Console.Write("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите второе число: ");
