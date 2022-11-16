0078032591dfbdedaba9862dcebb59f8fec0c82a
BatmanovSergeydotnet <xoolly@mail.ru>
2022-11-17 00:24:27 +0300

    Add Task 43 и корректировка Task 41

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,4 +1,49 @@
-﻿// Задача 41
+﻿// Задача 41: Домашняя работа
+// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Пример
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
 
-int num = 1%2;
-Console.WriteLine(num);
+int[] CreateArrayRndInt(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Введите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int PositiveElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Введите количество чисел для проверки: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateArrayRndInt(m);
+int positiveElements = PositiveElements (arr);
+
+Console.Write("Введёные числа для проверки: ");
+PrintArray(arr);
+Console.Write($"Количество введёных положительнх чисел равно {positiveElements}");
---
+++ Task43/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 43: 
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями: 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// Пример:
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Решение
+
+double[] CoordinatesPoint(double n1, double m1, double n2, double m2)
+{
+    double[] array = new double[2];
+    double x = (n2 - n1) / (m1 - m2);
+    double y = (m1 * x) + n1;
+    array[0] = x;
+    array[1] = y;
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+
+Console.WriteLine("Введите значения для первой прямой: ");
+Console.Write("b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значения для второй прямой: ");
+Console.Write("b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+if (b1 != b2 && k1 == k2) Console.WriteLine("Заданные прямые не имеют общих точек. Они параллельны друг другу");
+else if (b1 == b2 && k1 == k2) Console.WriteLine("Заданные прямые коллинеарны");
+else
+{
+    double[] arr = CoordinatesPoint(b1, k1, b2, k2);
+    Console.Write($"Координаты общей точки заданных прямых: ");
+    PrintArray(arr);
+}
+
