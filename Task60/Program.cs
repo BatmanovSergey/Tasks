093d10810c6bc59694d80fbfddd3356d9165ade2
BatmanovSergeydotnet <xoolly@mail.ru>
2022-11-28 18:14:33 +0300

    корректировка

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -12,24 +12,25 @@ void PrintMatrix3D(int[,,] matrix)
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         Console.WriteLine();
-        Console.Write("|");
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            for (int k = 0; k < matrix.GetLength(2); k++)
+            for (int k = 0; k < matrix.GetLength(2)-1; k++)
             {
-                if (k < matrix.GetLength(2)) Console.Write($"{matrix[i, j, k],3} ({i},{j},{k}) |");
+                if (k < matrix.GetLength(2)) Console.Write($"{matrix[i, j, k],3} ({i,2},{j,2},{k,2}); ");
+                else Console.Write($"{matrix[i, j, k],3} ({i,2},{j,2},{k,2})");
             }
         }
     }
 }
 
-int[] CreateArray(int size, int min)
+int[] CreateArray()
 {
-    int[] array = new int[size];
-    for (int i = 0; i < size; i++)
+    int[] array = new int[180];
+    int count = -99;
+    for (int i = 0; i < 180; i++)
     {
-        array[i] = min;
-        min++;
+        if (count == -9) count = 10;
+        array[i] = count++;
     }
     return array;
 }
@@ -37,8 +38,8 @@ int[] CreateArray(int size, int min)
 int[,,] CreateMatrix3D(int rows, int columns, int depth)
 {
     Random rnd = new Random();
-    int arrayRandomIndex = 0;
-    int[] oneArray = CreateArray(199, -99);
+    int arrayRandomIndex = default;
+    int[] oneArray = CreateArray();
     int[,,] matrix = new int[rows, columns, depth];
 
     for (int i = 0; i < matrix.GetLength(0); i++)
@@ -47,17 +48,13 @@ int[,,] CreateMatrix3D(int rows, int columns, int depth)
         {
             for (int k = 0; k < matrix.GetLength(2); k++)
             {
-                arrayRandomIndex = rnd.Next(89, 199);
-                if (oneArray[arrayRandomIndex] < 10)
+                arrayRandomIndex = rnd.Next(0, 180);
+                while (oneArray[arrayRandomIndex] == 0)
                 {
-                    arrayRandomIndex = rnd.Next(0, 89);
+                    arrayRandomIndex = rnd.Next(0, 180);
                 }
-                if (matrix[i, j, k] == 0)
-                {
-                    matrix[i, j, k] = oneArray[arrayRandomIndex];
-                    oneArray[arrayRandomIndex] = matrix[i, j, k];
-                }
-                
+                matrix[i, j, k] = oneArray[arrayRandomIndex];
+                oneArray[arrayRandomIndex] = 0;
             }
         }
     }
@@ -68,15 +65,15 @@ Console.Write("Введите первую размерность массива
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите вторую размерность массива:: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите третьюю размерность массива:: ");
+Console.Write("Введите третью размерность массива:: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
 
 int[,,] matr = CreateMatrix3D(num1, num2, num3);
 if (matr.Length <= 180)
 {
-    Console.WriteLine("Наш массив случайных чисел: ");
+    Console.WriteLine("Трёхмерный массив из не повторяющихся двузначных чисел: ");
     PrintMatrix3D(matr);
     Console.WriteLine();
 }
 else Console.WriteLine("Размер данного массива не позволяет "
-                     + "заполнить его неповторяющимися двузначными числами");
+                     + "заполнить его не повторяющимися двузначными числами");
--- Task62/Program.cs
+++ Task62/Program.cs
@@ -5,3 +5,67 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+
+int[,] CreateMatrixSnail(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    int count = 1;
+    int startCol = 0;
+    int endCol = columns - 1;
+    int startRow = 0;
+    int endRow = rows - 1;
+    while (startCol <= endCol && startRow <= endRow)
+    {
+        for (int i = startCol; i <= endCol; i++)
+        {
+            matrix[startRow, i] = count;
+            count++;
+        }
+        startRow++;
+
+        for (int i = startRow; i <= endRow; i++)
+        {
+            matrix[i, endCol] = count;
+            count++;
+        }
+        endCol--;
+
+        for (int i = endCol; i >= startCol; i--)
+        {
+            matrix[endRow, i] = count;
+            count++;
+        }
+        endRow--;
+
+        for (int i = endRow; i >= startRow; i--)
+        {
+            matrix[i, startCol] = count;
+            count++;
+        }
+        startCol++;
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3} |");
+            else Console.Write($"{matrix[i, j],3}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+Console.Write("Введите количество строк в массиве: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int[,] createMatrixSnail = CreateMatrixSnail(num1, num2);
+PrintMatrix(createMatrixSnail);
